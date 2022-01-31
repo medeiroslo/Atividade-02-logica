2efec8990bc6e4fff3e77df9d112c42e92fd7b9c
medeiroslo <medeirosslorrane@gmail.com>
2022-01-31 15:15:19 -0300

    Activity 02

---
+++ Atividade 02-logica/script.js
@@ -0,0 +1,17 @@
+var idade=18
+if(idade>=18){
+    console.log("Parabés,você tem 18 anos")
+}
+if (idade) {
+    console.log("Você é maior de idade!!!")
+}
+var humano=true;
+if (humano) {
+    console.log(true)
+    
+}
+var aniversário=true;
+if (aniversário) {
+    console.log("Você é aniversariante do mês de dezembro,parabéns!")
+    
+}
